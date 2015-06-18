e34c73e720ed428e48aa6d7125790e0980a03094
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-06-18 15:20:37 +0200

    bo extension basis

---
+++ app-extension/rubedomongodb/controller/RubedoMongoController.js
@@ -0,0 +1,11 @@
+
+
+Ext.define('Rubedo.controller.RubedoMongoController', {
+    extend: 'Ext.app.Controller',
+    alias: 'controller.RubedoMongoController',
+
+    init: function(application) {
+        console.log("test");
+    }
+
+});
--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,13 @@
 <?php
 return array(
+    /*
+      * BO extension definition
+      */
+    'appExtension' => array(
+        'rubedomongodb' => array(
+            'basePath' => realpath(__DIR__ . '/../app-extension') . '/rubedomongodb',
+            'definitionFile' => realpath(__DIR__ . '/../app-extension') . '/rubedomongodb/rubedomongodb.json'
+        )
+    ),
 
 );
