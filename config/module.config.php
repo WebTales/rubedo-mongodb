d4ed4c16e00ef94af3d7272e65c36797ac81b640
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-06-18 16:31:30 +0200

    isolated mappings collection

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,6 @@
 <?php
 return array(
+
     /*
       * BO extension definition
       */
@@ -8,6 +9,62 @@ return array(
             'basePath' => realpath(__DIR__ . '/../app-extension') . '/rubedomongodb',
             'definitionFile' => realpath(__DIR__ . '/../app-extension') . '/rubedomongodb/rubedomongodb.json'
         )
+    ),
+
+    /*
+     * Service definitions
+     */
+    'service_manager' => array(
+        'invokables' => array(
+            'RubedoMongoDB\\Collection\\MongoDBMappings' => 'RubedoMongoDB\\Collection\\MongoDBMappings',
+        ),
+        'aliases' => array(
+            'API\\Collection\\MongoDBMappings' => 'RubedoMongoDB\\Collection\\MongoDBMappings',
+            'MongoDBMappings' => 'RubedoMongoDB\\Collection\\MongoDBMappings',
+        ),
+    ),
+
+    /*
+     * Backoffice controller definitions
+     */
+    'controllers' => array(
+        'invokables' => array(
+            'RubedoMongoDB\\Backoffice\\Controller\\MongodbMappings' => 'RubedoMongoDB\\Backoffice\\Controller\\MongodbMappingsController',
+        ),
+    ),
+
+    /*
+    *  Backoffice route definitions
+    */
+    'router' => array (
+        'routes' => array(
+            'mongodb-mappings' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/backoffice/mongodb-mappings',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'RubedoMongoDB\\Backoffice\\Controller',
+                        'controller' => 'mongodb-mappings',
+                        'action' => 'index'
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type' => 'Segment',
+                        'options' => array(
+                            'route' => '/[:action]',
+                            '__NAMESPACE__' => 'RubedoMongoDB\\Backoffice\\Controller',
+                            'constraints' => array(
+                                'controller' => 'mongodb-mappings',
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*'
+                            ),
+                            'defaults' => array()
+                        )
+                    )
+                )
+            ),
+        ),
     ),
 
 );
